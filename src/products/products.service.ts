f8e59fd56fd10c784a1b8cc7625ba13c522db307
Pannaga2K <pannaga1123@gmail.com>
2021-07-07 16:04:34 +0530

    add nestjs e1

---
+++ src/products/products.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from "@nestjs/common";
+import {Product} from "./products.model";
+
+@Injectable()
+export class ProductsService {
+    products: Product[] = [];
+    insertProduct(title: string, desc: string, price: number) {
+        const prodId = new Date().toString();   
+        const newProduct = new Product(prodId, title, desc, price);
+        console.log(newProduct);
+        this.products.push(newProduct);
+        return prodId;
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+}
